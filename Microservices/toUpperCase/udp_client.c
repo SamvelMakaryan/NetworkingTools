f01c39e09fa0f1c3f8574838a4322f78cfbb0484
SamvelMakaryan <samvelmakaryan721@gmail.com>
2023-07-22 19:34:29 +0400

    update

--- Microservices/toUpperCase/udp_client.c
+++ Microservices/toUpperCase/udp_client.c
@@ -1,35 +1,10 @@
-/*
- * MIT License
- *
- * Copyright (c) 2018 Lewis Van Winkle
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-#include "chap04.h"
+#include "requirments.h"
 
 #if defined(_WIN32)
 #include <conio.h>
 #endif
 
 int main(int argc, char *argv[]) {
-
 #if defined(_WIN32)
     WSADATA d;
     if (WSAStartup(MAKEWORD(2, 2), &d)) {
@@ -37,12 +12,10 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 #endif
-
     if (argc < 3) {
         fprintf(stderr, "usage: udp_client hostname port\n");
         return 1;
     }
-
     printf("Configuring remote address...\n");
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
@@ -52,8 +25,6 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
-
-
     printf("Remote address is: ");
     char address_buffer[100];
     char service_buffer[100];
@@ -62,8 +33,6 @@ int main(int argc, char *argv[]) {
             service_buffer, sizeof(service_buffer),
             NI_NUMERICHOST);
     printf("%s %s\n", address_buffer, service_buffer);
-
-
     printf("Creating socket...\n");
     SOCKET socket_peer;
     socket_peer = socket(peer_address->ai_family,
@@ -72,8 +41,6 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
-
-
     printf("Connecting...\n");
     if (connect(socket_peer,
                 peer_address->ai_addr, peer_address->ai_addrlen)) {
@@ -81,28 +48,22 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     freeaddrinfo(peer_address);
-
     printf("Connected.\n");
     printf("To send data, enter text followed by enter.\n");
-
     while(1) {
-
         fd_set reads;
         FD_ZERO(&reads);
         FD_SET(socket_peer, &reads);
 #if !defined(_WIN32)
         FD_SET(0, &reads);
 #endif
-
         struct timeval timeout;
         timeout.tv_sec = 0;
         timeout.tv_usec = 100000;
-
         if (select(socket_peer+1, &reads, 0, 0, &timeout) < 0) {
             fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
             return 1;
         }
-
         if (FD_ISSET(socket_peer, &reads)) {
             char read[4096];
             int bytes_received = recv(socket_peer, read, 4096, 0);
@@ -113,7 +74,6 @@ int main(int argc, char *argv[]) {
             printf("Received (%d bytes): %.*s",
                     bytes_received, bytes_received, read);
         }
-
 #if defined(_WIN32)
         if(_kbhit()) {
 #else
@@ -125,15 +85,12 @@ int main(int argc, char *argv[]) {
             int bytes_sent = send(socket_peer, read, strlen(read), 0);
             printf("Sent %d bytes.\n", bytes_sent);
         }
-    } //end while(1)
-
+    }
     printf("Closing socket...\n");
     CLOSESOCKET(socket_peer);
-
 #if defined(_WIN32)
     WSACleanup();
 #endif
-
     printf("Finished.\n");
     return 0;
 }
